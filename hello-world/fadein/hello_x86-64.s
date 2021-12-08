b72a9470726eda79b512888c07f751a08573dad6
Erik on Voyager2 <ewfalor@gmail.com>
2021-12-07 20:58:26 -0700

    x86-64 and ARM assembly

---
+++ hello-world/fadein/hello_x86-64.s
@@ -0,0 +1,18 @@
+global _start
+
+section .text
+
+_start:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, world!\n",
+  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  syscall           ; );
+
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "x86_64 Assembly: Hello, world!", 10
+  msglen: equ $ - msg
