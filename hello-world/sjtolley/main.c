f9f66296127af1ec0c76481b5cc96ff321cb24a3
Sterling Tolley <sterlingj.tolley@gmail.com>
2021-12-08 18:22:41 -0700

    Added initial defines for hello world

---
+++ hello-world/sjtolley/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define OPEN_BRACKET {
+#define CLOSE_BRACKET }
+#define OPEN_PAREN (
+#define CLOSE_PAREN )
+#define OPEN_SQUARE
+#define CLOSE_SQUARE
+#define STAR *
+#define ASSIGN =
+#define SEMICOLON ;
+#define COMMENT(ignore)
+#define ZERO 0
+
+int main ( int argc, char * * argv) {
+
+	return ZERO;
+}
