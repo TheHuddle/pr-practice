d7381f9868e5b48634a0b0c48095de0e1e9d382f
Sterling Tolley <sterlingj.tolley@gmail.com>
2021-12-08 18:47:18 -0700

    Expanded string definition to maximize defines, fixed faulty defines SQUARE

--- hello-world/sjtolley/main.c
+++ hello-world/sjtolley/main.c
@@ -4,8 +4,8 @@
 #define CLOSE_BRACKET }
 #define OPEN_PAREN (
 #define CLOSE_PAREN )
-#define OPEN_SQUARE
-#define CLOSE_SQUARE
+#define OPEN_SQUARE [
+#define CLOSE_SQUARE ]
 #define STAR *
 #define ASSIGN =
 #define SEMICOLON ;
@@ -14,7 +14,9 @@
 #define COMMA ,
 
 int main OPEN_PAREN int argc COMMA char STAR STAR argv CLOSE_PAREN OPEN_BRACKET
-	char STAR word = "hello, world.";
-	puts(word);
+	char phrase OPEN_SQUARE CLOSE_SQUARE ASSIGN OPEN_BRACKET 'h' COMMA 'e' COMMA 'l' COMMA 'l' COMMA
+		'o' COMMA ',' COMMA ' ' COMMA 'w' COMMA 'o' COMMA 'r' COMMA 'l' COMMA 'd' COMMA '.' COMMA 0 CLOSE_BRACKET SEMICOLON
+	char STAR word ASSIGN phrase SEMICOLON
+	puts OPEN_PAREN word CLOSE_PAREN SEMICOLON
 	return ZERO;
 CLOSE_BRACKET
